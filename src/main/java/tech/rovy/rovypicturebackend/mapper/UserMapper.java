ff15ea8cd89fd040a82c3837820e48eb6cd657d0
LPtrichor <1985835442@qq.com>
2024-12-11 13:43:21 +0800

    添加用户模块，包括注册、登录、注销等功能

---
+++ src/main/java/tech/rovy/rovypicturebackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package tech.rovy.rovypicturebackend.mapper;
+
+import tech.rovy.rovypicturebackend.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author rovy
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2024-12-10 14:42:23
+* @Entity tech.rovy.rovypicturebackend.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
